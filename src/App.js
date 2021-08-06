81e559c6cccf2581e3f7f5e157d7929c09ddd6f5
Danish Virani <danishvirani.tech@gmail.com>
2021-08-06 17:58:33 -0400

    new user form created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+import './App.css'
+
+import NewUser from './components/NewUser'
+
+const App = () => {
+
+  let [newEmail, setNewEmail] = useState('')
+  let [newPassword, setNewPassword] = useState('')
+  let [newFirstName, setNewFirstName] = useState('')
+  let [newLastName, setNewLastName] = useState('')
+  let [newPronouns, setNewPronouns] = useState('')
+  let [newFaveClass, setNewFaveClass] = useState('')
+  let [newProfilePic, setNewProfilePic] = useState('')
+  let [newFriends, setNewFriends] = useState('')
+  let [users, setUsers] = useState([])
+
+  let newStates = {
+        newEmail:newEmail,
+        newPassword:newPassword,
+        newFirstName:newFirstName,
+        newLastName:newLastName,
+        newPronouns:newPronouns,
+        newFaveClass:newFaveClass,
+        newProfilePic:newProfilePic,
+        newFriends:newFriends
+    }
+
+    const changeHandlers = {
+        emailChange: (e) => {
+            setNewEmail(e.target.value)
+        },
+        passwordChange: (e) => {
+            setNewPassword(e.target.value)
+        },
+        firstChange: (e) => {
+            setNewFirstName(e.target.value)
+        },
+        lastChange: (e) => {
+            setNewLastName(e.target.value)
+        },
+        pronounsChange: (e) => {
+            setNewPronouns(e.target.value)
+        },
+        picChange: (e) => {
+            setNewProfilePic(e.target.value)
+        },
+        classChange: (e) => {
+            setNewFaveClass(e.target.value)
+        }
+    }
+
+    const getUsers = () => {
+        axios
+            .get('https://dndateme-backend.herokuapp.com/users')
+            .then((response) => {
+                setUsers(response.data)
+            })
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <NewUser
+    changeHandlers={changeHandlers}
+    newStates={newStates}
+    getUsers={getUsers}
+    />
+    </>
+  )
+
 }
 
 export default App;
