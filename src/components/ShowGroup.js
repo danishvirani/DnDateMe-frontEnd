74fd718a184cb0119250c56a69fe1778764b0e69
Danish Virani <danishvirani.tech@gmail.com>
2021-08-11 12:13:39 -0400

    bug fixes

--- src/components/ShowGroup.js
+++ src/components/ShowGroup.js
@@ -4,13 +4,20 @@ import axios from 'axios'
 
 const ShowGroup = (props) => {
 
-  let group = props.showGroup
+  let [newName, setNewName] = useState('')
+  let [newLocation, setNewLocation] = useState('')
+  let [newImage, setNewImage] = useState('')
+  let [group, setGroup] = useState(props.showGroup)
+
+  const handleNewName = (event) => {
+    setNewName(event.target.value)
+  }
+  const handleNewImage = (event) => {
+    setNewImage(event.target.value)
+  }
 
   const accept = (request) => {
-    axios
-      .put(
-        `https://dndateme-backend.herokuapp.com/groups/${group._id}/${request}/accept`)
-      .then(props.getGroups())
+
   }
 
   const deny = (request) => {
@@ -18,6 +25,31 @@ const ShowGroup = (props) => {
       .put(
         `https://dndateme-backend.herokuapp.com/groups/${group._id}/${request}/deny`)
       .then(props.getGroups())
+  }
+
+  const handleEditForm = (event) => {
+      event.preventDefault()
+      axios
+        .put(
+          `https://dndateme-backend.herokuapp.com/groups/${group._id}`,
+          {
+            name:newName || group.name,
+            image:newImage || group.image
+          }
+        ).then(refreshGroup())
+
+  }
+
+  const toggleEditGroup = () => {
+    document.querySelector('#groupForm').classList.toggle('hidden')
+  }
+
+  const refreshGroup = () => {
+    axios
+        .get(`https://dndateme-backend.herokuapp.com/groups/${group._id}`)
+        .then((response) => {
+            setGroup(response.data)
+        })
   }
 
   // {group.members.map((member) => {
@@ -30,24 +62,45 @@ const ShowGroup = (props) => {
     return(
         <>
         {
-          (props.currentUser._id == group.admin)?
-          group.joinRequests.map((request) => {
-            return (
-              <>
-              {props.users.filter(user => user._id == request).map(filteredName => (
-                <p>{filteredName.firstName + ' ' + filteredName.lastName}</p>
-              ))}
-              <button onClick={accept(request)}>Accept</button>
-              <button onClick={deny(request)}>Deny</button>
-              </>
-            )
-          })
-          :
-          <></>
+          (props.currentUser == undefined)?
+            <></>
+            :
+            (props.currentUser._id == group.admin)?
+            group.joinRequests.map((request) => {
+              return (
+                <>
+                {props.users.filter(user => user._id == request).map(filteredName => (
+                  <p>{filteredName.firstName + ' ' + filteredName.lastName}</p>
+                ))}
+                <button onClick={accept(request)}>Accept</button>
+                <button onClick={deny(request)}>Deny</button>
+                </>
+              )
+            })
+            :
+            <></>
         }
         <h1>{group.name}</h1>
         <img src={group.image} alt={group.name}/>
         <h4>Members: </h4>
+
+        {
+          (props.currentUser == undefined)?
+            <></>
+            :
+            (props.currentUser._id == group.admin)?
+              <>
+              <button onClick={toggleEditGroup}>Edit Group</button>
+              <form class='hidden' id='groupForm' onSubmit={(event) => handleEditForm(event)}>
+                <label htmlFor="name">Group Name: </label>
+                <input type="text" onChange={handleNewName}/>
+                <label htmlFor="image">Group Image: </label>
+                <input type="text" onChange={handleNewImage}/>
+                <input type="submit" value="Update Group"/>
+              </form>
+              </>
+            : <></>
+        }
 
         </>
     )
